6a16762915603108f68072e5fada50827118146c
Hiền Trần <ht800423@gmail.com>
2025-03-22 10:24:11 +0700

    pagitation

--- controllers/admin/product_controller.js
+++ controllers/admin/product_controller.js
@@ -3,6 +3,7 @@ const Product = require("../../models/product.model");
 
 const filterStatusHelper = require("../../Helper/filterStatus");
 const SearchHelper = require("../../Helper/search");
+const PagitationHelper = require("../../Helper/pagitation");
 // [Get] / admin / products;
 module.exports.index = async (req, res) => {
   // FilterSatus
@@ -17,8 +18,6 @@ module.exports.index = async (req, res) => {
 
   // Search
   const objectSearch = SearchHelper(req.query);
-  // console.log(objectSearch);
-
   if (objectSearch.regex) {
     // keyword = req.query.keyword;
     // const regex = new RegExp(keyword, "i");
@@ -27,24 +26,16 @@ module.exports.index = async (req, res) => {
   //End Search
 
   const countProducts = await Product.countDocuments(find);
-
-  // console.log(countProducts);
   //Pagitation
-  let objectPagitation = {
-    limitItem: 4,
-    currentPage: 1,
-  };
-  if (req.query.page) {
-    objectPagitation.currentPage = parseInt(req.query.page);
-  }
+  let objectPagitation = PagitationHelper(
+    req.query,
+    {
+      limitItem: 4,
+      currentPage: 1,
+    },
+    countProducts
+  );
 
-  objectPagitation.skip =
-    (objectPagitation.currentPage - 1) * objectPagitation.limitItem;
-  // console.log(objectPagitation.currentPage);
-  const totalPage = Math.ceil(countProducts / objectPagitation.limitItem);
-  // console.log(totalPage);
-
-  objectPagitation.totalPage = totalPage;
   //End Patitation
   const products = await Product.find(find)
     .limit(objectPagitation.limitItem)
